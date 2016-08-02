6ee5d033e3d1528b4c07114c2cc871e2fcb63352
Evgeny Klimenchenko <zklimenchenko@gmail.com>
2016-08-02 14:09:58 +0000

    added database

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ var express = require("express");
 var session = require('express-session');
 var request = require("request");
 var url = require('url');
+var mongo = require('mongodb').MongoClient;
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+var DBurl = `mongodb://${process.env.DBUser}:${process.env.DBPassword}@ds139715.mlab.com:39715/twitch_users`;
+
 var sessions = [];
+mongo.connect(DBurl, function(err, db) {
+    var users = db.collection('users');
+});
 
 var channel = '';
 app.use(session({secret: 'test', path: '*', resave: true, saveUninitialized: false, httpOnly: true})); //create sessionID
+
 app.get('/auth/user', function(req, res) { //Authanticate user
-    console.log(req.sessionID);
     var query = url.parse(req.originalUrl).query.split('&');
     query = query.map(function(element) {
         return element.split('=')[1];
-    })
+    });
     if(query[2] == 'test') {
         request({
             url: 'https://api.twitch.tv/kraken/oauth2/token',
@@ -31,64 +38,84 @@ app.get('/auth/user', function(req, res) { //Authanticate user
             }
         }, function(err, response, body) {
             if(err) throw err;
-            if(JSON.parse(body).access_token) {
-                var hour = 3600000
-                req.session.value = req.sessionID;
-                req.session.cookie.expires = new Date(Date.now() + hour)
-                req.session.cookie.maxAge = hour
-                console.log(req.session.cookie.maxAge);
-                request({
-                    url: 'https://api.twitch.tv/kraken/user',
-                    method: 'GET',
-                    headers: {
-                        Accept: 'application/vnd.twitchtv.v3+json',
-                        Authorization: 'OAuth '+JSON.parse(body).access_token
-                    }
-                }, function(err, response, body) {
-                    if(err) throw err;
-                    var indexOfUser = sessions.findIndex(function(element) {
-                        if(element.name == JSON.parse(body).name) {
-                            return true;
-                        } else return false;
-                    })
-                    if(indexOfUser === -1) {
-                        sessions.push({name: JSON.parse(body).name, sessionID: req.sessionID, polls: {}});
-                    } else {
-                        if(sessions[indexOfUser].sessionID !== req.sessionID) {
-                            sessions[indexOfUser].sessionID = req.sessionID;
+            if(JSON.parse(body).access_token) {//Authorisation got through
+                mongo.connect(DBurl, function(err, db) {
+                    var users = db.collection('users');
+                    request({
+                        url: 'https://api.twitch.tv/kraken/user',
+                        method: 'GET',
+                        headers: {
+                            Accept: 'application/vnd.twitchtv.v3+json',
+                            Authorization: 'OAuth '+JSON.parse(body).access_token
                         }
-                    }
-                
-                    res.send(sessions)
-                })
+                    }, function(err, response, body) {
+                        if(err) throw err;
+                        users.findOne({name: JSON.parse(body).name}, function(err, user) {
+                            if (user === null) {
+                                users.count({}, function(err, id) {//getting next id
+                                    users.insert({ _id: id, name: JSON.parse(body).name, sessionID: req.sessionID, polls: {}});
+                                });
+                            } else {
+                                users.updateOne({name: JSON.parse(body).name}, { $set: {sessionID: req.sessionID}});
+                            }
+                        });
+                        req.session.name = JSON.parse(body).name;
+                        res.writeHead(302, {'Location': req.session.url});
+                        res.end();
+                    });
+                });   
+            } else {
+                res.writeHead(302, {'Location': req.session.url});
+                res.end();
             }
-        })
+        });
     }
     
-})
+});
+
+
 app.get('/:channel', function(req, res) {
-    console.log(req.params.channel)
+    if(req.session.name) {
+        req.params.name = req.session.name;
+        console.log(req.params.name);
+    }
     if (req.params.channel === 'favicon.ico') {
         res.writeHead(200, {'Content-Type': 'image/x-icon'} );
         res.end();
         console.log('favicon requested');
         return;
     }
+    var hour = 3600000;
+    req.session.cookie.expires = new Date(Date.now() + hour);
+    req.session.cookie.maxAge = hour;
+    req.session.url = req.protocol + 's://' + req.get('host') + req.originalUrl;
+    console.log(req.session.url);
     channel = req.params.channel;
     res.sendFile(__dirname + '/Public/index.html');
      
 });
+
+
 app.get('/', function(req,res) {
     res.sendFile(__dirname + '/Public/channel.html');
 });
+
+
 app.use(express.static(__dirname+'/Public', {index: '_'}));
+
+
 
 
 io.on('connection', function(socket) {
     var data = {channel: channel, count: 0};
     socket.emit('option', data);
-    console.log('user connected')
+    console.log('user connected');
 });
+
+
+
+
+
 
 var PORT = process.env.PORT || 8080;
 

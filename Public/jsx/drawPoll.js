b2f41b0d0854f9c1eb13bcfdc7b3203b8640a277
Evgeny Klimenchenko <zklimenchenko@gmail.com>
2016-08-11 08:31:07 +0000

    updated log in butotn

--- Public/jsx/drawPoll.js
+++ Public/jsx/drawPoll.js
@@ -7,7 +7,7 @@ function drawPoll(poll) {
     var votes = [];
     var piesvg = d3.select('#pie').append('svg')
         .attr('height', '400')
-        .attr('width', '600')
+        .attr('width', '500')
         .append('g')
         .attr('transform', 'translate('+pieDim.w/2+','+pieDim.h/2+')');
     var arc = d3.arc().outerRadius(pieDim.w/2 - 10).innerRadius(0);
